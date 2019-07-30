8385e3e55b0500e1999b1f58920040f6f4d74150
Sebastian Peña <spena@azuan.com>
2019-07-30 18:13:23 -0500

    uso del modelo (Model) y jstl (uso y configuracion)

--- src/main/java/net/itinajero/app/controller/HomeController.java
+++ src/main/java/net/itinajero/app/controller/HomeController.java
@@ -1,15 +1,49 @@
 package net.itinajero.app.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
+	
 	@RequestMapping(value="/home", method=RequestMethod.GET)
-	public String goHome(){
+	public String goHome() {
 		return "home";
+	}
+	
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String mostrarPrincipal(Model model) {
+		
+		List<String> peliculas = new LinkedList<>();
+		peliculas.add("Rapido y Furioso");
+		peliculas.add("El aro 2");
+		peliculas.add("Aliens");
+		
+		model.addAttribute("peliculas", peliculas);
+		
+		return "home";
+	}
+	
+	
+	@RequestMapping(value="/detail")
+	public String mostrarDetalle(Model model) {
+		
+		String tituloPelicula = "Rapidos y Furiosos";
+		int duracion = 136;
+		double precioEntrada = 50;
+		
+		model.addAttribute("titulo", tituloPelicula);
+		model.addAttribute("duracion", duracion);
+		model.addAttribute("precio", precioEntrada);
+		
+		return "detalle";
 	}
 	
 }
