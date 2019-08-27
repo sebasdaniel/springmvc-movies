9dc37c1adb6bb386edfdd33b4b102551516fdd21
Sebastian <pe.sebas@gmail.com>
2019-08-26 21:45:14 -0500

    Se suben y guardan las imagenes de la pelicula

--- src/main/java/net/itinajero/app/controller/PeliculasController.java
+++ src/main/java/net/itinajero/app/controller/PeliculasController.java
@@ -1,8 +1,12 @@
 package net.itinajero.app.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -15,6 +19,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import net.itinajero.app.model.Pelicula;
@@ -46,13 +52,19 @@ public class PeliculasController {
 	
 	
 	@PostMapping("/save")
-	public String guardar(Pelicula pelicula, BindingResult result, RedirectAttributes attributes) {
+	public String guardar(Pelicula pelicula, BindingResult result, RedirectAttributes attributes,
+			@RequestParam("archivoImagen") MultipartFile multiPart, HttpServletRequest request) {
 		
 		if (result.hasErrors()) {
 			return "peliculas/formPelicula";
 //			for (ObjectError error: result.getAllErrors()) {
 //				System.out.println("Error: " + error.getDefaultMessage());
 //			}
+		}
+		
+		if (!multiPart.isEmpty()) {
+			String nombreImage = guardarImagen(multiPart, request);
+			pelicula.setImagen(nombreImage);
 		}
 		
 		System.out.println("Guardando: " + pelicula);
@@ -66,6 +78,31 @@ public class PeliculasController {
 	}
 	
 	
+	private String guardarImagen(MultipartFile multiPart, HttpServletRequest request) {
+		
+		// Obtenemos el nombre original del archivo
+		String nombreOriginal = multiPart.getOriginalFilename();
+		
+		// Obtenemos la ruta ABSOLUTA del directorio images
+		// apache-tomcat/webapps/cineapp/resources/images/
+		String rutaFinal = request.getServletContext().getRealPath("/resources/images/");
+		
+		try {
+			// Formamos el nombre del archivo para guardarlo en el disco duro
+			File imageFile = new File(rutaFinal + nombreOriginal);
+			
+			// Aqui se guarda fisicamente el archivo en el disco duro
+			multiPart.transferTo(imageFile);
+			
+			return nombreOriginal;
+			
+		} catch (IOException e) {
+			System.out.println("Error " + e.getMessage());
+			return null;
+		}
+	}
+
+
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
