7e4b0deced6a82cb60ce714ed5b20457ab26ee00
Sebastian <pe.sebas@gmail.com>
2019-09-11 22:06:06 -0500

    Se cambia la interfaz que hereda NoticiasRespository a JpaRepository

--- src/main/java/net/itinajero/app/repository/NoticiasRepository.java
+++ src/main/java/net/itinajero/app/repository/NoticiasRepository.java
@@ -1,11 +1,11 @@
 package net.itinajero.app.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import net.itinajero.app.model.Noticia;
 
 @Repository
-public interface NoticiasRepository extends CrudRepository<Noticia, Integer> {
+public interface NoticiasRepository extends JpaRepository<Noticia, Integer> {
 
 }
