f010c6ad1a27c09bd17a3d5f6af8ec2dbc91342a
Sebastian <pe.sebas@gmail.com>
2019-09-15 12:30:39 -0500

    Se establecen metodos findBy para Estatus y fechas

--- src/main/java/net/itinajero/app/repository/NoticiasRepository.java
+++ src/main/java/net/itinajero/app/repository/NoticiasRepository.java
@@ -1,5 +1,8 @@
 package net.itinajero.app.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,9 @@ import net.itinajero.app.model.Noticia;
 
 @Repository
 public interface NoticiasRepository extends JpaRepository<Noticia, Integer> {
+	
+	List<Noticia> findByEstatus(String status);
+	
+	List<Noticia> findByFecha(Date fecha);
 
 }
