57cda7bbaa35a82b63adf2db1da23d33ae6f3454
Sebastian <pe.sebas@gmail.com>
2019-09-19 22:10:57 -0500

    Se configura anotacion ManyToOne para los Horarios

--- src/main/java/net/itinajero/app/model/Horario.java
+++ src/main/java/net/itinajero/app/model/Horario.java
@@ -6,8 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "Horarios")
@@ -21,7 +22,8 @@ public class Horario {
 	private String sala; 
 	private double precio;
 	
-	@Transient
+	@ManyToOne
+	@JoinColumn(name = "idPelicula")
 	private Pelicula pelicula;
 
 	public Horario() { }
