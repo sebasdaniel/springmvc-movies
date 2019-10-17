b3a312cde649cc54d684eec81ee90d3e17e990a3
Sebastian <pe.sebas@gmail.com>
2019-10-16 21:14:16 -0500

    Se muestra en consola nombre de usuario logueado y roles con proposito informativo

--- src/main/java/net/itinajero/app/controller/LoginController.java
+++ src/main/java/net/itinajero/app/controller/LoginController.java
@@ -2,6 +2,8 @@ package net.itinajero.app.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +15,16 @@ public class LoginController {
 	
 	
 	@GetMapping("index")
-	public String mostrarBienvenida() {
+	public String mostrarBienvenida(Authentication authentication) {
+		
+		// nombre del usuario
+		System.out.println("Usuario: " + authentication.getName());
+		
+		// roles del usuario
+		for (GrantedAuthority rol : authentication.getAuthorities()) {
+			System.out.println("Rol: " + rol.getAuthority());
+		}
+		
 		return "admin";
 	}
 
